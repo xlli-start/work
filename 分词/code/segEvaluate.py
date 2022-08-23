047a4637e7ee34690d9ea05f4427b4424f00d14e
xianglong_li <319131492@qq.com>
2022-08-24 00:05:09 +0800

    前向最大匹配分词

--- 分词/code/segEvaluate.py
+++ 分词/code/segEvaluate.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python3
+from unittest import result
 from config import para
 import argparse
 
 
-def loaddict():
-    wordsdict = set()
+def loadDict():
+    wordsDict = set()
     with open(para["dict"], "r", encoding="utf-8") as fp:
         for word in fp:
-            wordsdict.add(word.strip())
-    return wordsdict
+            wordsDict.add(word.strip())
+    return wordsDict
 
+def segSentence():
+    wordsDict = loadDict()
+    sens = list()
+    with open(para["sentence"], "r", encoding="utf-8") as fs:
+        for sen in fs:
+            sen = sen.strip()
+            endIndex = para["wordlen"]
+            beginIndex = 0
+            words = list()
+            while beginIndex < len(sen):
+                word = sen[beginIndex:endIndex]
+                if word in wordsDict:
+                    words.append(word)
+                    beginIndex += len(word)
+                    endIndex = beginIndex + para["wordlen"]
+                elif len(word) == 1:
+                    words.append(word)
+                    beginIndex += 1
+                    endIndex = beginIndex + para["wordlen"]
+                else:
+                    endIndex -= 1
+            sens.append(words)
+                
 
 def main(args):
+    segSentence()
 
 
 if __name__ == "__main__":
